379d39e60d8ca1b68275080048c8236f53264579
zaid khan <zaiddkhhan@gmail.com>
2023-04-24 13:40:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recipeapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.recipeapp.utils
+
+object Constants {
+
+        const val BASE_URL = "https://api.spoonacular.com/"
+
+        const val API_KEY = "8f8ff59792f54f5cae099782592f1a14"
+
+}
